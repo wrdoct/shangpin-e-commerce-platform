c1c19a2d3ef5d4c7b7c3034cdf325bda0707aa2b
llw <llw18729357726@163.com>
2024-01-27 16:20:52 +0800

    用户登录接口和统一异常处理

--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/service/impl/SysUserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.atguigu.spzx.manager.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.atguigu.spzx.common.exception.GuiguException;
 import com.atguigu.spzx.manager.mapper.SysUserMapper;
 import com.atguigu.spzx.manager.service.SysUserService;
 import com.atguigu.spzx.model.dto.system.LoginDto;
 import com.atguigu.spzx.model.entity.system.SysUser;
+import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import com.atguigu.spzx.model.vo.system.LoginVo;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,13 +33,15 @@ public class SysUserServiceImpl implements SysUserService {
         // 根据用户名查询用户
         SysUser sysUser = sysUserMapper.selectByUserName(loginDto.getUserName());
         if(sysUser == null) {
-            throw new RuntimeException("用户名或者密码错误") ;
+//            throw new RuntimeException("用户名不存在") ;
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
         }
         // 验证密码是否正确
         String inputPassword = loginDto.getPassword();
         String md5InputPassword = DigestUtils.md5DigestAsHex(inputPassword.getBytes());
         if(!md5InputPassword.equals(sysUser.getPassword())) {
-            throw new RuntimeException("用户名或者密码错误") ;
+//            throw new RuntimeException("用户名或者密码错误") ;
+            throw new GuiguException(ResultCodeEnum.LOGIN_ERROR);
         }
         // 生成令牌，保存数据到Redis中 // key: token    value: 用户信息
         String token = UUID.randomUUID().toString().replaceAll("-", "");

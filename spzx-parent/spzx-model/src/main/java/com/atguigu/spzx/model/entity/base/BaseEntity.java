31d925e5e0bd28a7a882c23b66162673b13d7bb5
llw <llw18729357726@163.com>
2024-02-03 18:35:23 +0800

    创建时间和修改时间问题优化

--- spzx-parent/spzx-model/src/main/java/com/atguigu/spzx/model/entity/base/BaseEntity.java
+++ spzx-parent/spzx-model/src/main/java/com/atguigu/spzx/model/entity/base/BaseEntity.java
@@ -7,17 +7,24 @@ import lombok.Data;
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * 说明：mysql数据库运行在docker容器中，所以要修改mysql容器的时区为Asia/Shanghai。
+ * docker exec -it mysql bash
+ * ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ * exit
+ * docker restart mysql
+ */
 @Data
 public class BaseEntity implements Serializable {
 
     @Schema(description = "唯一标识")
     private Long id;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @Schema(description = "创建时间")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @Schema(description = "修改时间")
     private Date updateTime;
 

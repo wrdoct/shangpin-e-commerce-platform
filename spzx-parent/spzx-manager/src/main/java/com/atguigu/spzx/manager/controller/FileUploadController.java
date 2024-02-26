5a5622b1bec2c9958c9cf6fa82e1139c51febc44
llw <llw18729357726@163.com>
2024-02-26 11:46:17 +0800

    文件上传接口

--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/FileUploadController.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/FileUploadController.java
@@ -3,6 +3,8 @@ package com.atguigu.spzx.manager.controller;
 import com.atguigu.spzx.manager.service.FileUploadService;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "文件上传接口")
 @RestController
 @RequestMapping("/admin/system")
 public class FileUploadController {
@@ -18,6 +21,7 @@ public class FileUploadController {
     private FileUploadService fileUploadService ;
 
     // <input type="file" name="file">
+    @Operation(summary = "文件上传")
     @PostMapping(value = "/fileUpload")
     public Result<String> fileUploadService(@RequestParam(value = "file") MultipartFile multipartFile) {
         String fileUrl = fileUploadService.fileUpload(multipartFile) ;

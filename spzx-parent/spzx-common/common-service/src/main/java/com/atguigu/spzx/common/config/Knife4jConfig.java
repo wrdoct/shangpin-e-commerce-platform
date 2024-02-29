a2d43b0de5dfc2bb439d5e3a833ae6d5f328b885
llw <llw18729357726@163.com>
2024-02-29 19:16:26 +0800

    使用knife4j进行调试时将token设置到请求头

--- spzx-parent/spzx-common/common-service/src/main/java/com/atguigu/spzx/common/config/Knife4jConfig.java
+++ spzx-parent/spzx-common/common-service/src/main/java/com/atguigu/spzx/common/config/Knife4jConfig.java
@@ -1,8 +1,11 @@
 package com.atguigu.spzx.common.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +20,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Knife4jConfig {
 
+    private final static String authTokenHeaderName = "token";  // 替换为实际用于身份验证的请求头名称
+
     @Bean
     public GroupedOpenApi adminApi() {      // 创建了一个api接口的分组
         return GroupedOpenApi.builder()
                 .group("admin-api")         // 分组名称
                 .pathsToMatch("/admin/**")  // 接口请求路径规则
+                .addOperationCustomizer((operation, handlerMethod) -> {
+//                    // 获取请求路径
+//                    String requestMappingPath = "/admin" + handlerMethod.getMethod().getAnnotation(RequestMapping.class).value()[0];
+//                    // 使用 antPath 匹配路径--排除登录接口和生成验证码接口
+//                    if (new AntPathMatcher().match("/admin/system/index/login", requestMappingPath)
+//                            || new AntPathMatcher().match("/admin/system/index/generateValidateCode", requestMappingPath)) {
+//                        return operation;
+//                    }
+                    operation.addSecurityItem(new SecurityRequirement().addList(authTokenHeaderName));
+                    return operation;
+                })
                 .build();
     }
 
@@ -36,6 +52,13 @@ public class Knife4jConfig {
                         .title("尚品甑选API接口文档")
                         .version("1.0")
                         .description("尚品甑选API接口文档")
-                        .contact(new Contact().name("llw"))); // 设定作者
+                        .contact(new Contact().name("llw"))) // 设定作者
+                .components(new Components()
+                        .addSecuritySchemes(authTokenHeaderName, new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .name(authTokenHeaderName)
+                                .in(SecurityScheme.In.HEADER)))
+                .addSecurityItem(new SecurityRequirement().addList(authTokenHeaderName));
     }
+
 }

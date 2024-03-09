f8c08f669db7412ee219dad3f8a2a9038040f342
llw <llw18729357726@163.com>
2024-03-09 20:53:34 +0800

    添加作者

--- spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
+++ spzx-parent/spzx-manager/src/main/java/com/atguigu/spzx/manager/ManagerApplication.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @author llw
+ */
 @EnableAsync
 @EnableLogAspect
 @EnableScheduling

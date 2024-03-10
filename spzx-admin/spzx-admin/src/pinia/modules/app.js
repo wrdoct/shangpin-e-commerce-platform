bd14be74f69b670551c2604e153833c7ff451903
llw <llw18729357726@163.com>
2024-03-10 19:09:24 +0800

    前端左侧菜单栏的标题调整

--- spzx-admin/spzx-admin/src/pinia/modules/app.js
+++ spzx-admin/spzx-admin/src/pinia/modules/app.js
@@ -22,7 +22,7 @@ const COLLAPSE = 'VEA-COLLAPSE'
 
 export const useApp = defineStore('app', {
   state: () => ({
-    title: 'Vue3 Element Admin',
+    title: '尚品甄选', // 'Vue3 Element Admin',
     authorization: getItem(TOKEN),
     sidebar: {
       collapse: getItem(COLLAPSE),
